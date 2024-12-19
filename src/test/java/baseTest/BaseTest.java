642c1a929fd9b4f248a01583ccb741c661fb489f
Yehia.Metwally <jmetwallym@gmail.com>
2024-12-20 01:13:40 +0200

    Test

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,4 +1,34 @@
 package baseTest;
 
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+
+import static yehiaEngine.browserActions.WindowManager.closeAllWindows;
+import static yehiaEngine.browserActions.WindowManager.navigateToURL;
+import static yehiaEngine.driverManager.BrowserFactory.getDriver;
+import static yehiaEngine.driverManager.BrowserFactory.openBrowser;
+import static yehiaEngine.managers.PropertiesManager.getPropertiesValue;
+
 public class BaseTest {
+    //Variables
+    protected ThreadLocal<RemoteWebDriver> isolatedDriver;
+
+    @BeforeMethod
+    public void setUpAndOpenBrowser() throws MalformedURLException {
+        //Open Browser
+        isolatedDriver = openBrowser();
+
+        //Navigate to Website URL
+        navigateToURL(getDriver(isolatedDriver),getPropertiesValue("baseUrlWeb"));
+    }
+
+    @AfterMethod
+    public void closeBrowser()
+    {
+        //Close All Browser Tabs
+        closeAllWindows(getDriver(isolatedDriver));
+    }
 }
